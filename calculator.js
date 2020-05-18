307543d36acfc39091a232ca0f2b8cfef789e49f
Jake-Hurley <jshurley97@gmail.com>
2020-05-19 11:37:14 +1200

    add files and pseudocode to calculator.js

---
+++ calculator.js
@@ -0,0 +1,29 @@
+//pseudo code
+
+//variables
+// a total that stores the final value of whats being calculated.
+
+// an array that stores all of the entries put into the calculator.
+
+// a temporary variable that stores the current number or symbol.(needs on click function)
+
+//
+
+//functions
+//
+// when a button is pressed and when it is pressed, run a function that sets a var or val to the values string ('5' or 'x')
+
+// run a function to check for a number in value and adds it to temp. (if)
+// set an else if that checks for a symbol other than equals. adds it to temp. 
+//  
+// CLEAR LAST ENTRY: checks if val = 'CE'. Sets temp to "".
+// CHECK FOR MULTIPLY: check if val = 'x'. Adds temp to entries array and then adds *.
+// CHECK FOR DIVIDE: check if val = '÷'. Adds temp to entries array and then adds /.
+// CHECK FOR EQUALS: check if val = '='. Adds temp to entries array.
+
+// write a function that runs through all the data inside entries as an equation?
+//  create var for the next number and the symbol. 
+//  check symbol and set the equation for symbol and next number.
+// swap the - symbol so that text input handles it correctly. 
+
+// return answer on equals but also clear entries and temp.
